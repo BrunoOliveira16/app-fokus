7bb755d23a9c94bc0b9c5155da6a49127fb4da3d
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2025-05-08 18:43:09 -0300

    feat: created mock

---
+++ mock/index.ts
@@ -0,0 +1,20 @@
+export const pomodoro = [
+  {
+    id: "focus",
+    initialValue: 25,
+    image: require("./assets/pomodoro.png"),
+    display: "Foco",
+  },
+  {
+    id: "short",
+    initialValue: 5,
+    image: require("./assets/short.png"),
+    display: "Pausa curta",
+  },
+  {
+    id: "long",
+    initialValue: 15,
+    image: require("./assets/long.png"),
+    display: "Pausa longa",
+  },
+];
