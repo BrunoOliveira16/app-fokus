0d23477e5e31cc802e60eca751dd5ee128fa747a
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2025-05-08 21:13:55 -0300

    feat: modified mock and added logic for timer control

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,61 @@
-import { useState } from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { useRef, useState } from "react";
+import { View, StyleSheet, Image, ImageSourcePropType } from "react-native";
 
 import { Timer } from "@/components/Timer";
 import { Footer } from "@/components/Footer";
 import { FokusButton } from "@/components/FokusButton";
 import { ActionButton } from "@/components/ActionButton";
+import { IconPause, IconPlay } from "@/components/Icons";
 
 import { pomodoro } from "@/mock/index";
 
+type timerTypeProps = {
+  id: string;
+  initialValue: number;
+  image: ImageSourcePropType;
+  display: string;
+};
+
 export default function Index() {
-  const [timerType, setTimerType] = useState(pomodoro[0]);
+  const [timerType, setTimerType] = useState<timerTypeProps>(pomodoro[0]);
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue);
+  const [timerRunning, setTimerRunning] = useState(false);
+
+  const timerRef = useRef<number | null>(null);
+
+  const clear = () => {
+    if (timerRef.current !== null) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+      setTimerRunning(false);
+    }
+  };
+
+  const toogleTimerType = (newTimerType: timerTypeProps) => {
+    setTimerType(newTimerType);
+    setSeconds(newTimerType.initialValue);
+    clear();
+  };
+
+  const toogleTimer = () => {
+    if (timerRef.current) {
+      clear();
+      return;
+    }
+
+    setTimerRunning(true);
+
+    const id = setInterval(() => {
+      setSeconds((oldState) => {
+        if (oldState === 0) {
+          clear();
+          return timerType.initialValue;
+        }
+        return oldState - 1;
+      });
+    }, 1000) as unknown as number;
+    timerRef.current = id;
+  };
 
   return (
     <View style={styles.container}>
@@ -22,13 +68,17 @@ export default function Index() {
               key={item.id}
               title={item.display}
               active={timerType.id === item.id}
-              handlePress={() => setTimerType(item)}
+              handlePress={() => toogleTimerType(item)}
             />
           ))}
         </View>
 
-        <Timer totalSeconds={timerType.initialValue} />
-        <FokusButton />
+        <Timer totalSeconds={seconds} />
+        <FokusButton
+          title={timerRunning ? "Pausar" : "Começar"}
+          icon={timerRunning ? <IconPause /> : <IconPlay />}
+          handlePress={toogleTimer}
+        />
       </View>
       <Footer />
     </View>
--- mock/index.ts
+++ mock/index.ts
@@ -2,19 +2,19 @@ export const pomodoro = [
   {
     id: "focus",
     initialValue: 25,
-    image: require("./assets/pomodoro.png"),
+    image: require("../app/assets/pomodoro.png"),
     display: "Foco",
   },
   {
     id: "short",
     initialValue: 5,
-    image: require("./assets/short.png"),
+    image: require("../app/assets/short.png"),
     display: "Pausa curta",
   },
   {
     id: "long",
     initialValue: 15,
-    image: require("./assets/long.png"),
+    image: require("../app/assets/long.png"),
     display: "Pausa longa",
   },
 ];
